0bdf4b71b8fc93488921c657ca7c2ecb2cc8dbe8
Michael Ehab <michaelehab16@gmail.com>
2022-06-08 11:14:18 +0200

    Added Day 8 Solution

---
+++ 1332.Remove-Palindromic-Subsequences.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int removePalindromeSub(string s) {
+        int i = 0, j = s.length() - 1;
+        while(i < j) if (s[i++] != s[j--]) return 2;
+        return 1;
+    }
+};
